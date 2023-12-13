0307715f01a2295550a618c7c765f75ee728f957
Zynkah <ffgurl07@yahoo.com>
2023-12-13 15:02:29 -0600

    Fix: useEffect added

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import { useSession } from "next-auth/react";
 import SignInButton from "@/components/login-btn";
 import SignOutButton from "@/components/logout-btn";
@@ -9,7 +9,17 @@ import SignOutButton from "@/components/logout-btn";
 
 export default function Home() {
   const { data: session } = useSession();
-  const [loggedIn, setLoggedIn] = useState();
+  const [loggedIn, setLoggedIn] = useState(false);
+
+    useEffect(() => {
+      const userIsLoggedIn = session?.user?.email;
+      if (typeof userIsLoggedIn === "string") {
+        setLoggedIn(true);
+      } else {
+        setLoggedIn(false);
+      }
+    }, [session]);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
