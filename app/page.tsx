cc9d754161098025c31bc2a10e258daa03604d4e
Zynkah <ffgurl07@yahoo.com>
2023-12-20 13:12:34 -0600

    Feat: light and dark mode button added

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { useSession } from "next-auth/react";
 import SignInButton from "@/components/login-btn";
 import SignOutButton from "@/components/logout-btn";
+import { ModeToggle } from "@/components/light-dark-mode";
 
 
 
@@ -13,9 +14,13 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <div className="m-auto border rounded p-2 w-max">
-        {session ? <SignOutButton /> : <SignInButton />}
-</div>
+        <div className="fixed right-2 top-2 hover:scale-110 hover:bg-slate-900">
+          <ModeToggle />
+        </div>
+
+        <div className="m-auto border rounded w-max hover:scale-110 hover:bg-slate-900">
+          {session ? <SignOutButton /> : <SignInButton />}
+        </div>
       </div>
     </main>
   );
