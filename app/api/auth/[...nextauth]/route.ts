34e6252cf1c644c6ac97ec5a79462fa957c2064f
Zynkah <ffgurl07@yahoo.com>
2023-12-20 19:20:19 -0600

    Fix: build fix

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,34 +1,5 @@
-import NextAuth, { type NextAuthOptions } from "next-auth";
-import EmailProvider from "next-auth/providers/email";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
-
-export const authOptions: NextAuthOptions = {
-  adapter: PrismaAdapter(prisma),
-  secret: process.env.NEXTAUTH_SECRET,
-  providers: [
-    EmailProvider({
-      server: {
-        host: process.env.EMAIL_SERVER_HOST,
-        port: Number(process.env.EMAIL_SERVER_PORT),
-        auth: {
-          user: process.env.EMAIL_SERVER_USER,
-          pass: process.env.EMAIL_SERVER_PASSWORD,
-        },
-      },
-      from: process.env.EMAIL_FROM,
-    }),
-  ],
-  theme: {
-    colorScheme: "auto", // "auto" | "dark" | "light"
-    brandColor: "#914cd2", // Hex color code
-    logo: "https://next-auth.js.org/img/logo/logo-sm.png", // Absolute URL to image
-    buttonText: "#d3d9ea", // Hex color code
-  },
-};
+import NextAuth from "next-auth";
+import { authOptions } from "@/lib/auth";
 
 const handler = NextAuth(authOptions);
-
 export { handler as GET, handler as POST };
